1c8945e3dde0081bbc1a8f3d0fe40582da6469e6
blacix <blacix@hotmail.com>
2021-03-28 13:22:11 +0200

    misc

--- xsmApp.cpp
+++ xsmApp.cpp
@@ -7,12 +7,12 @@
 
 
 xsmApp::xsmApp() {
-  xsmAppEndpoint ep(std::bind(&xsmApp::onMessageReceived, this, std::placeholders::_1));
+  xsmAppEndpoint endpoint(std::bind(&xsmApp::onMessageReceived, this, std::placeholders::_1));
 
   xsm::Message msg1{{'a', 'b', 'c'}, 3};
   xsm::Message msg2{{'c', 'd', 'e'}, 3};
-  ep.send(msg1);
-  ep.send(msg2);
+  endpoint.send(msg1);
+  endpoint.send(msg2);
 }
 
 void xsmApp::onMessageReceived(xsm::Message message) {
